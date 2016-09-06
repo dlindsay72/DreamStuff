e80e2776a74a912c986e353831d86941bb9643cf
Dan Lindsay <dlindsay72@hotmail.com>
2016-09-06 13:01:56 -0400

    Data model done

---
+++ DreamStuff/Image+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Image+CoreDataClass.swift
+//  DreamStuff
+//
+//  Created by Dan Lindsay on 2016-09-06.
+//  Copyright © 2016 Dan Lindsay. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Image: NSManagedObject {
+
+}
---
+++ DreamStuff/ItemType+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ItemType+CoreDataClass.swift
+//  DreamStuff
+//
+//  Created by Dan Lindsay on 2016-09-06.
+//  Copyright © 2016 Dan Lindsay. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class ItemType: NSManagedObject {
+
+}
