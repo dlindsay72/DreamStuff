c594d17f868b14aebbf48a3f59acd5a98afa8fd5
Dan Lindsay <dlindsay72@hotmail.com>
2016-09-06 14:20:36 -0400

    Views setup done

--- DreamStuff/MainVC.swift
+++ DreamStuff/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  DreamStuff
 //
 //  Created by Dan Lindsay on 2016-09-06.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
