90bd525750122456734eaffdf04f7b9432fa8fea
Dan Lindsay <dlindsay72@hotmail.com>
2016-09-06 19:33:25 -0400

    fetch and display data working

--- DreamStuff/ItemCell.swift
+++ DreamStuff/ItemCell.swift
@@ -12,9 +12,14 @@ class ItemCell: UITableViewCell {
 
     @IBOutlet weak var thumb: UIImageView!
     @IBOutlet weak var titleLbl: UILabel!
-    
     @IBOutlet weak var priceLbl: UILabel!
     @IBOutlet weak var detailsLbl: UILabel!
+    
+    func configureCell(item: Item) {
+        titleLbl.text = item.title
+        priceLbl.text = "$\(item.price)0"
+        detailsLbl.text = item.details
+    }
     
     
     
