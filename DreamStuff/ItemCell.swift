18d6e86fe72e583585f1946e9d4a7923389ccefb
Dan Lindsay <dlindsay72@hotmail.com>
2016-09-06 14:30:19 -0400

    created outlets and ItemCell class

---
+++ DreamStuff/ItemCell.swift
@@ -0,0 +1,21 @@
+//
+//  ItemCell.swift
+//  DreamStuff
+//
+//  Created by Dan Lindsay on 2016-09-06.
+//  Copyright © 2016 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    
+    @IBOutlet weak var priceLbl: UILabel!
+    @IBOutlet weak var detailsLbl: UILabel!
+    
+    
+    
+}
