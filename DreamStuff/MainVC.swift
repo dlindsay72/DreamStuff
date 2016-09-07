68e952aab08c7aac21fbb4fd4212fe0cd1b1b139
Dan Lindsay <dlindsay72@hotmail.com>
2016-09-07 16:57:13 -0400

    can now save data from the Add/Edit page

--- DreamStuff/MainVC.swift
+++ DreamStuff/MainVC.swift
@@ -23,7 +23,7 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         tableView.delegate = self
         tableView.dataSource = self
         
-        generateTestData()
+    //    generateTestData()
         attemptFetch()
     }
 
@@ -75,6 +75,8 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         fetchRequest.sortDescriptors = [dateSort]
         
         let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        
+        controller.delegate = self
         
         self.controller = controller
         
